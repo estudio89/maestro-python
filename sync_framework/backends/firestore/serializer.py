32186f107c12eb0c6bda009058c3109354021357
Luccas Correa <luccascorrea@estudio89.com.br>
2021-06-29 19:56:23 -0300

    Add field serialization methods to allow for customization

--- sync_framework/backends/firestore/serializer.py
+++ sync_framework/backends/firestore/serializer.py
@@ -10,6 +10,14 @@ import datetime as dt
 
 
 class FirestoreItemSerializer(BaseItemSerializer):
+    def serialize_field_value(
+        self, collection_name: "str", item: "Dict[str, Any]", key="str"
+    ) -> "Any":
+        value = item[key]
+        if isinstance(value, dt.datetime) or isinstance(value, dt.date):
+            value = value.isoformat()
+        return value
+
     def serialize_item(self, item: "Dict[str, Any]") -> "str":
         pk = item["id"]
         collection_name = item["collection_name"]
@@ -19,10 +27,9 @@ class FirestoreItemSerializer(BaseItemSerializer):
             if key in ["id", "collection_name"]:
                 continue
 
-            value = item[key]
-            if isinstance(value, dt.datetime) or isinstance(value, dt.date):
-                value = value.isoformat()
-
+            value = self.serialize_field_value(
+                collection_name=collection_name, item=item, key=key
+            )
             fields[key] = value
 
         table_name = collection_to_table_name(collection=collection_name)
@@ -30,6 +37,29 @@ class FirestoreItemSerializer(BaseItemSerializer):
         serialized_data = dict(sorted(serialized_data.items()))
         serialized_item = json.dumps(serialized_data)
         return serialized_item
+
+    def deserialize_field_value(
+        self, collection_name: "str", fields: "Dict[str, Any]", key: "str"
+    ) -> "Any":
+
+        value = fields[key]
+        if value is None:
+            return None
+
+        try:
+            date = dt.date.fromisoformat(value)
+            return date
+        except (ValueError, TypeError):
+            pass
+
+        # checking for datetime
+        try:
+            date = parse_datetime(value)
+            return date
+        except (ValueError, TypeError):
+            pass
+
+        return value
 
     def deserialize_item(self, serialized_item: "str") -> "Dict[str, Any]":
         raw_data = json.loads(serialized_item)
@@ -40,18 +70,9 @@ class FirestoreItemSerializer(BaseItemSerializer):
         item = {"id": pk, "collection_name": collection_name}
 
         for key in fields:
-            item[key] = fields[key]
-            # checking for date
-            try:
-                date = dt.date.fromisoformat(item[key])
-                item[key] = date
-            except (ValueError, TypeError):
-                pass
+            value = self.deserialize_field_value(
+                collection_name=collection_name, fields=fields, key=key
+            )
+            item[key] = value
 
-            # checking for datetime
-            try:
-                date = parse_datetime(item[key])
-                item[key] = date
-            except (ValueError, TypeError):
-                pass
         return item
