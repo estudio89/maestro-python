bd66ec83a2e8fa4c9d693fcaa8192d2812c6d1eb
Luccas Correa <luccascorrea@estudio89.com.br>
2021-07-18 22:47:40 -0300

    Initial implementation of query feature for in_memory backend

--- maestro/backends/in_memory/store.py
+++ maestro/backends/in_memory/store.py
@@ -1,5 +1,6 @@
 from maestro.core.store import BaseDataStore
 from maestro.core.query import Query
+from maestro.backends.in_memory.utils import query_filter_to_lambda
 from maestro.core.metadata import (
     VectorClock,
     ItemChange,
@@ -30,7 +31,11 @@ class InMemoryDataStore(BaseDataStore):
 
     def get_local_vector_clock(self, query: "Optional[Query]" = None) -> "VectorClock":
         vector_clock = VectorClock.create_empty(provider_ids=[self.local_provider_id])
-        for item_change in self.get_item_changes():
+        item_changes = self.get_item_changes()
+        filtered_item_changes = self._filter_item_changes(
+            item_changes=item_changes, query=query
+        )
+        for item_change in filtered_item_changes:
             vector_clock.update_vector_clock_item(
                 provider_id=item_change.provider_id,
                 timestamp=item_change.provider_timestamp,
@@ -70,9 +75,12 @@ class InMemoryDataStore(BaseDataStore):
             if item_change.provider_timestamp > remote_timestamp:
                 selected_changes.append(item_change)
 
-        selected_changes = copy.deepcopy(selected_changes)
+        filtered_changes = self._filter_item_changes(
+            item_changes=selected_changes, query=query
+        )
+        filtered_changes = copy.deepcopy(filtered_changes)
         return self._paginate_item_changes(
-            all_changes=selected_changes, max_num=max_num
+            all_changes=filtered_changes, max_num=max_num
         )
 
     def select_deferred_changes(
@@ -94,9 +102,12 @@ class InMemoryDataStore(BaseDataStore):
                 if remote_timestamp < item_change.provider_timestamp:
                     selected_changes.append(conflict_log.item_change_loser)
 
-        selected_changes = copy.deepcopy(selected_changes)
+        filtered_changes = self._filter_item_changes(
+            item_changes=selected_changes, query=query
+        )
+        filtered_changes = copy.deepcopy(filtered_changes)
         return self._paginate_item_changes(
-            all_changes=selected_changes, max_num=max_num
+            all_changes=filtered_changes, max_num=max_num
         )
 
     def save_item_change(
@@ -169,12 +180,118 @@ class InMemoryDataStore(BaseDataStore):
         )
         self._save(item=sync_session_record, key="sync_sessions")
 
+    def _get_previous_item_change(
+        self, item_id: "str", provider_id: "str", timestamp: "dt.datetime"
+    ) -> "Optional[ItemChange]":
+        max_item_change = None
+        max_vector_clock_item = None
+
+        for item_change in self.get_item_changes():
+            # NOT SURE IF THE COMPARISON BELOW MAKES SENSE
+            # I think it will be necessary to implement comparison between VectorClocks
+            vector_clock_item = item_change.vector_clock.get_vector_clock_item(
+                provider_id=provider_id
+            )
+            if (
+                item_change.item_id == item_id
+                and item_change.is_applied
+                and not item_change.should_ignore
+                and vector_clock_item.timestamp < timestamp
+            ):
+                if (
+                    max_vector_clock_item is None
+                    or vector_clock_item > max_vector_clock_item
+                ):
+                    max_vector_clock_item = vector_clock_item
+                    max_item_change = item_change
+
+        return max_item_change
+
+    def _filter_item_changes(
+        self, item_changes: "List[ItemChange]", query: "Optional[Query]"
+    ):
+        if query:
+            filter_lambda = query_filter_to_lambda(filter=query.filter)
+            sorted_changes = item_changes[:]
+
+            # Sorting
+            for sort_order in reversed(query.ordering):
+                sorted_changes.sort(
+                    lambda item_change: self.deserialize_item(
+                        item_change.serialized_item
+                    )[sort_order.field_name],
+                    reverse=sort_order.descending,
+                )
+
+            # Filtering
+            item_ids_in_current_query: "List[str]" = []
+            all_item_ids: "Set[str]" = set()
+            extra_item_change_ids = []
+            for item_change in sorted_changes:
+                item = self.deserialize_item(item_change.serialized_item)
+                in_query = filter_lambda(item)
+                if in_query:
+                    if item_change.item_id not in all_item_ids:
+                        all_item_ids.add(item_change.item_id)
+
+                    current_version = self.get_item_version(item_id=item_change.item_id)
+                    if current_version.current_item_change.id == item_change.id:
+                        item_ids_in_current_query.append(item_change.item_id)
+                else:
+                    # NEED SOME WAY TO CHECK WHETHER THE ITEM WAS PREVIOUSLY IN THE QUERY
+                    # BECAUSE IF IT WAS, THEN THIS CHANGE NEEDS TO BE SENT ALONG.
+                    # AT THE SAME TIME, THE FACT THAT IT ONCE WAS PART OF THE QUERY
+                    # IS NOT ENOUGH, OTHERWISE YOU'D SEND ALONG ANY CHANGE TO AN
+                    # ITEM THAT WAS ONCE PART OF THE QUERY
+                    # >>> SHOULD ANALYZE THE LAST ITEM CHANGE TO THIS ITEM BEFORE THIS ONE
+                    # >>> AND INCLUDE THE CHANGE ONLY IF THAT LAST ITEM CHANGE WAS PART OF THE QUERY
+                    previous_change = self._get_previous_item_change(
+                        item_id=item_change.item_id,
+                        provider_id=item_change.provider_id,
+                        timestamp=item_change.provider_timestamp,
+                    )
+                    if not previous_change:
+                        continue
+                    item = self.deserialize_item(previous_change.serialized_item)
+                    in_query = filter_lambda(item)
+                    if in_query:
+                        extra_item_change_ids.append(item_change.id)
+
+            item_ids_not_in_current_query: "List[str]" = list(
+                set(all_item_ids).difference(set(item_ids_in_current_query))
+            )
+
+            # Pagination
+            start_idx = 0 if query.offset is None else query.offset
+            end_idx = (
+                len(item_ids_in_current_query)
+                if query.limit is None
+                else start_idx + query.limit
+            )
+            item_ids_in_current_query = item_ids_in_current_query[start_idx:end_idx]
+
+            # Selecting changes
+            filtered_item_ids = (
+                item_ids_not_in_current_query + item_ids_in_current_query
+            )
+            filtered_changes: "List[ItemChange]" = []
+            for item_change in item_changes:
+                if (
+                    item_change.item_id in filtered_item_ids
+                    or item_change.id in extra_item_change_ids
+                ):
+                    filtered_changes.append(item_change)
+        else:
+            filtered_changes = item_changes
+
+        return filtered_changes
+
     def get_item_changes(self) -> "List[ItemChange]":
+        changes = self._db["item_changes"]
+
         return [
             self.item_change_metadata_converter.to_metadata(record=item)
-            for item in sorted(
-                self._db["item_changes"], key=lambda value: value["date_created"]
-            )
+            for item in sorted(changes, key=lambda value: value["date_created"])
         ]
 
     def save_item(self, item: "Dict"):
