988cfd1b80d376db1be47a62ac6775edfe8b70b1
Luccas Correa <luccascorrea@estudio89.com.br>
2022-06-07 15:22:22 +0000

    Finish maestro setup in example app for django and fastAPI.

--- example2/django/todolist/core/sync.py
+++ example2/django/todolist/core/sync.py
@@ -1,8 +1,11 @@
 from maestro.backends.django.contrib.factory import create_django_provider
+from maestro.backends.mongo.contrib.factory import create_mongo_provider
+
 # from maestro.backends.firestore.contrib.factory import create_firestore_provider
 # import maestro.backends.firestore
 import maestro.backends.django
 from maestro.core.orchestrator import SyncOrchestrator
+
 # from firebase_admin import firestore
 import threading
 from typing import TYPE_CHECKING
@@ -12,22 +15,28 @@ def on_changes_committed():
     thread = threading.Thread(target=start_sync, args=["django"])
     thread.start()
 
-def start_sync(initial_source_provider_id: "str"):
-    print("WOULD SYNC")
 
-    # # Django
-    # django_provider = create_django_provider()
+def start_sync(initial_source_provider_id: "str"):
+
+    # Django
+    django_provider = create_django_provider()
+
+    # Mongo
+    mongo_provider = create_mongo_provider(
+        connect_uri="mongodb://maestro:maestro@10.222.0.5:27000/?authSource=admin&readPreference=primary&directConnection=true&ssl=false",
+        database_name="example-db",
+    )
 
     # # Firestore
     # firestore_provider = create_firestore_provider()
 
-    # # Sync lock
-    # sync_lock = maestro.backends.django.DjangoSyncLock()
+    # Sync lock
+    sync_lock = maestro.backends.django.DjangoSyncLock()
 
     # # Orchestrator
-    # orchestrator = SyncOrchestrator(
-    #     sync_lock=sync_lock,
-    #     providers=[django_provider, firestore_provider],
-    #     maximum_duration_seconds=10 * 60,
-    # )
-    # orchestrator.run(initial_source_provider_id=initial_source_provider_id)
+    orchestrator = SyncOrchestrator(
+        sync_lock=sync_lock,
+        providers=[django_provider, mongo_provider],
+        maximum_duration_seconds=10 * 60,
+    )
+    orchestrator.run(initial_source_provider_id=initial_source_provider_id)
