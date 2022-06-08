684e2db8255f608dbceda33213a66711f67df197
Luccas Correa <luccascorrea@estudio89.com.br>
2022-06-08 00:14:46 +0000

    Fixed initialization of firebase admin SDK in example app

--- example2/django/todolist/core/apps.py
+++ example2/django/todolist/core/apps.py
@@ -3,19 +3,3 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     name = 'core'
-
-    def ready(self):
-        import firebase_admin
-        from firebase_admin import credentials
-        from pathlib import Path
-        import os
-
-        # Build paths inside the project like this: BASE_DIR / 'subdir'.
-        BASE_DIR = Path(__file__).resolve().parent.parent
-        os.environ["FIRESTORE_EMULATOR_HOST"] = "0.0.0.0:7070"
-
-        directory = os.path.dirname(__file__)
-        cred = credentials.Certificate(
-            BASE_DIR / "serviceAccountKey.json"
-        )
-        firebase_admin.initialize_app(cred)
--- example2/django/todolist/core/sync.py
+++ example2/django/todolist/core/sync.py
@@ -10,6 +10,20 @@ from maestro.core.orchestrator import SyncOrchestrator
 import threading, sys, traceback
 from typing import TYPE_CHECKING
 
+import firebase_admin
+from firebase_admin import credentials
+from pathlib import Path
+import os
+
+# Initialize firebase SDK
+BASE_DIR = Path(__file__).resolve().parent.parent
+os.environ["FIRESTORE_EMULATOR_HOST"] = "0.0.0.0:7070"
+
+directory = os.path.dirname(__file__)
+cred = credentials.Certificate(
+    BASE_DIR / "serviceAccountKey.json"
+)
+firebase_admin.initialize_app(cred)
 
 class DebugEventsManager(EventsManager):
     def on_exception(
